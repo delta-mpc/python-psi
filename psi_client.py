15bcfefa645f7565fbd968c7b63329872c506d22
73902 <miaohong@yuanben.org>
2021-05-27 18:08:04 +0800

    [feature] add comment

--- psi_client.py
+++ psi_client.py
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     host = "127.0.0.1"
     local_port = 1234
     peer_port = 2345
-    local = Address(host, local_port)
-    peer = Address(host, peer_port)
+    local = Address(host, local_port)  # local address
+    peer = Address(host, peer_port)  # peer address
 
-    words = [i.to_bytes(4, "big") for i in range(1000)]
+    words = [i.to_bytes(4, "big") for i in range(1000)]  # local sets, should be a list of bytes
 
     with make_pair(local, peer) as pair:
-        client = Client(pair, words)
+        client = Client(pair, words)  # psi client
         logging.info("start prepare")
-        client.prepare()
+        client.prepare()  # prepare stage
         logging.info("finish prepare")
 
         logging.info("start intersection")
-        res = client.intersect()
+        res = client.intersect()  # intersect stage, res is the intersection
         logging.info("finish intersection")
         print(sorted([int.from_bytes(val, "big") for val in res]))
 
--- psi_server.py
+++ psi_server.py
@@ -13,19 +13,19 @@ if __name__ == '__main__':
     host = "127.0.0.1"
     local_port = 2345
     peer_port = 1234
-    local = Address(host, local_port)
-    peer = Address(host, peer_port)
+    local = Address(host, local_port)  # local address
+    peer = Address(host, peer_port)  # peer address
 
-    words = [i.to_bytes(4, "big") for i in range(500, 1000)]
+    words = [i.to_bytes(4, "big") for i in range(500, 1000)]  # local sets, should be a list of bytes
 
     with make_pair(local, peer) as pair:
-        server = Server(pair, words)
+        server = Server(pair, words)  # psi server
         logging.info("start prepare")
-        server.prepare()
+        server.prepare()  # prepare stage
         logging.info("finish prepare")
 
         logging.info("start intersection")
-        res = server.intersect()
+        res = server.intersect()  # intersect stage, res is the intersection
         logging.info("finish intersection")
         print(sorted([int.from_bytes(val, "big") for val in res]))
 
