ee10a2dbacf6c51e009f8997bcc4465408635516
miaohong <739025250@qq.com>
2022-02-24 11:08:10 +0800

    update version to 0.0.2

--- setup.py
+++ setup.py
@@ -4,17 +4,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="delta_psi",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     include_package_data=True,
     exclude_package_data={'': ['.gitignore']},
     install_requires=[
         'numpy~=1.20.2',
-        'pycryptodome~=3.10.1'
+        'pycryptodome~=3.10.1',
+        "pyyaml~=5.3"
     ],
     zip_safe=False,
     author="miaohong",
     author_email="miaohong@yuanben.org",
     description="package for private set intersection",
+    entry_points={
+        "console_scripts": [
+            "psi_run=psi.main:main"
+        ]
+    },
     python_requires='>=3.6',
 )
